c281151192c4e54e96972f29dcedb4eaf9a0d694
MiguelMartin <miguel92martin@gmail.com>
2021-03-25 15:58:23 +0100

    Registro y login básico con base de datos MongoDB

--- main.py
+++ main.py
@@ -3,22 +3,39 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from views import *
 
 app = Flask(__name__, static_url_path='/static')
-
+app.secret_key = 'BQ2S5Idd4C'
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/salir')
+def salir():
+    session.clear()
+    return render_template('index.html')
 
 @app.route('/iniciarSesion', methods=["GET", "POST"])
 def iniciarSesion():
-    datos = iniciar_sesion(request)
+    datos = {}
+    if request.form:
+        datos = iniciar_sesion(request)
+
+        if datos['logeado']:
+            return render_template('index.html')
+
     return render_template('iniciar_sesion.html', datos=datos)
 
 
 @app.route('/registrar', methods=["GET", "POST"])
 def registrar():
-    datos = registrar_usuario(request)
+    datos = {}
+
+    if request.form:
+        datos = registrar_usuario(request)
+
+        if datos['insertado']:
+            return render_template('index.html')
+
     return render_template('registrar.html', datos=datos)
 
 
