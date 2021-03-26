9d4f048a8e14d24fb804bb6ea33389ad51854e0a
Alejandro <alexgarohotmailcom@gmail.com>
2021-03-26 17:51:41 +0100

    updated

--- main_ejemplo.py
+++ main_ejemplo.py
@@ -8,11 +8,17 @@ from cryptography.fernet import Fernet
 import os
 import shutil
 import base64
+import pymongo as pymongo
 
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
 import os, binascii
 from backports.pbkdf2 import pbkdf2_hmac
+
+client = pymongo.MongoClient("mongodb+srv://spea:grupodetres@cluster0.uscnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+db = client['ServerFiles']
+coleccionUsuarios = db['Usuarios']
+coleccionFicheros = db['Ficheros']
 
 def convert_key(shared_key):
     derived_key = HKDF(
@@ -26,18 +32,7 @@ def convert_key(shared_key):
 
     return key
 
-def create_key_client(password):
-
-    salt = binascii.unhexlify('aaef2d3f4d77ac66e9c5a6c3d8f921d1')
-    password = password.encode("utf8")
-    key = pbkdf2_hmac("sha256", password, salt, 50000, 32)
-    
-    return binascii.hexlify(key)
-
-def get_key_client(client):
-    return None
-
-def encrypt_data_key(filename, key_client):
+def encrypt_data_key(key_client, filename):
 
     salt = binascii.unhexlify('aaef2d3f4d77ac66e9c5a6c3d8f921d1')
     password = filename.encode("utf8")
@@ -46,29 +41,45 @@ def encrypt_data_key(filename, key_client):
     f = Fernet(convert_key(key_client))
     return f.encrypt(key), key
 
-def encrypt_file(filename, key_client):
+def encrypt_file(client_name, filename):
 
-    with open(filename, "rb") as file:
+    upload_path = 'upload/' + filename
+    encrypted_path = 'encrypted/' + client_name
+    key_client = coleccionUsuarios.find_one({"correo": client_name})['key']
+
+    with open(upload_path, "rb") as file:
       file_contents = file.read()
 
-    data_key_encrypted, data_key_plaintext = encrypt_data_key(filename, key_client)
+    data_key_encrypted, data_key_plaintext = encrypt_data_key(key_client, upload_path)
 
     f = Fernet(convert_key(data_key_plaintext))
     file_contents_encrypted = f.encrypt(file_contents)
 
-    with open(filename + '.encrypted', 'wb') as file_encrypted:
-        file_encrypted.write(file_contents_encrypted)
+    os.makedirs(encrypted_path, exist_ok=True)
 
-    return data_key_encrypted
+    with open(encrypted_path + '/' + filename, 'wb') as file_encrypted:
+        file_encrypted.write(file_contents_encrypted)
+    
+    #DESCOMENTAR PARA LA FASE FINAL
+    #os.remove(upload_path)
+
+    coleccionFicheros.delete_many({"path": encrypted_path + '/' + filename})
+    fichero = {"client": client_name, "datakey": data_key_encrypted, "path": encrypted_path + '/' + filename}
+    coleccionFicheros.insert_one(fichero)
 
 def decrypt_data_key(data_key_encrypted, key_client):
 
     f = Fernet(convert_key(key_client))
     return f.decrypt(data_key_encrypted)
 
-def decrypt_file(filename, data_key_encrypted, key_client):
+def decrypt_file(client_name, filename):
 
-    with open(filename + ".encrypted", "rb") as file:
+    encrypted_path = 'encrypted/' + client_name
+    decrypted_path = 'download/' + client_name
+    key_client = coleccionUsuarios.find_one({"correo": client_name})['key']
+    data_key_encrypted = coleccionFicheros.find_one({"path": encrypted_path + "/" + filename})['datakey']
+
+    with open(encrypted_path + '/' + filename, "rb") as file:
       file_contents = file.read()
 
     data_key_plaintext = decrypt_data_key(data_key_encrypted, key_client)
@@ -76,90 +87,77 @@ def decrypt_file(filename, data_key_encrypted, key_client):
     f = Fernet(convert_key(data_key_plaintext))
     file_contents_decrypted = f.decrypt(file_contents)
 
-    with open(filename + '.decrypted', 'wb') as file_decrypted:
+    os.makedirs(decrypted_path, exist_ok=True)
+
+    with open(decrypted_path + '/' + filename, 'wb') as file_decrypted:
       file_decrypted.write(file_contents_decrypted)
 
-def change_files_with_new_keys(oldKeyClient, newKeyClient, data_key_encrypted, filename):
+def key_rotation(client_name):
 
-    decrypt_file(filename, data_key_encrypted, oldKeyClient)
-    data_key_encryptionNew=encrypt_file(filename, newKeyClient)
+    oldKeyClient = coleccionUsuarios.find_one({"correo": client_name})['key']
+    #MIGUEL: How to decode the password???
+    password = coleccionUsuarios.find_one({"correo": client_name})['password']
 
-    os.remove(filename + '.decrypted')
-    return data_key_encryptionNew
+    salt = binascii.unhexlify('aaef2d3f4d77ac66e9c5a6c3d8f921d1')
+    passwordTmp = password.encode("utf8")
+    newKeyClient = pbkdf2_hmac("sha256", passwordTmp, salt, 50000, 32)
 
-path = '.'
-path_secure = './secure/'
-path_unsecure = './unsecure/'
-NUM_BYTES_FOR_LEN = 4
+    coleccionUsuarios.update_one({"correo": client_name},{"$set": {"key": newKeyClient}})
 
-#Clean folders
-shutil.rmtree(path_secure,ignore_errors=True)
-shutil.rmtree(path_unsecure,ignore_errors=True)
-os.mkdir(path_secure)
-os.mkdir(path_unsecure)
+    encrypted_path = 'encrypted/' + client_name
+    keyrotation_path = 'keyrotation/' + client_name
 
-#List files in folder
-files = []
-# r=root, d=directories, f = files
-for r, d, f in os.walk(path):
-    for file in f:
-        if '.txt' in file:
-            files.append(os.path.join(r, file))
+    for filename in os.listdir(encrypted_path):
 
-for f in files:
-    print(f)
+        data_key_encrypted = coleccionFicheros.find_one({"path": encrypted_path + "/" + filename})['datakey']
 
-chunk_size = 256
+        with open(encrypted_path + '/' + filename, "rb") as file:
+            file_contents = file.read()
+      
+        data_key_plaintext = decrypt_data_key(data_key_encrypted, oldKeyClient)
 
-#Use a static Master Key to protect all files.
-nonce = bytes("0123456789012345",'utf-8')
-key = bytes("01234567890123456789012345678901",'utf-8')
+        f = Fernet(convert_key(data_key_plaintext))
+        file_contents_decrypted = f.decrypt(file_contents)
 
-fileNameMiguel= "ejemplo.txt"
+        os.makedirs(keyrotation_path, exist_ok=True)
 
-keyClient = create_key_client("test")
-keyFile = encrypt_file(fileNameMiguel, keyClient)
+        with open(keyrotation_path + '/' + filename, 'wb') as file_decrypted:
+            file_decrypted.write(file_contents_decrypted)
 
-keyClient2 = create_key_client("test")
-newKeyFile = change_files_with_new_keys(keyClient,keyClient2,keyFile,fileNameMiguel)
-decrypt_file(fileNameMiguel,newKeyFile,keyClient2)
+        os.remove(encrypted_path + '/' + filename)
 
-#key2,test2 = create_cmk("miguel")
-#key3,test3 = create_cmk("alex")
+        with open(keyrotation_path + '/' + filename, "rb") as file:
+            file_contents = file.read()
+        
+        data_key_encrypted, data_key_plaintext = encrypt_data_key(newKeyClient, keyrotation_path)
 
-#keyFileMiguel = encrypt_file(fileNameMiguel, key2)
-#keyFileAlex = encrypt_file(fileNameAlex, key3)
+        f = Fernet(convert_key(data_key_plaintext))
+        file_contents_encrypted = f.encrypt(file_contents)
 
-#decrypt_file(fileNameMiguel, keyFileMiguel, key2)
-#decrypt_file(fileNameAlex, keyFileAlex, key3)
+        with open(encrypted_path + '/' + filename, 'wb') as file_encrypted:
+            file_encrypted.write(file_contents_encrypted)
+        
+        os.remove(keyrotation_path + '/' + filename)
 
-#decrypt_file(fileName, keyFile)
+        coleccionFicheros.update_one({"path": encrypted_path + "/" + filename},{"$set": {"datakey": data_key_encrypted}})
 
-#Ferment doesn't implemt the update pattern, so I am using a stream cipher instead.
-algorithm = algorithms.ChaCha20(key, nonce)
-cipher = Cipher(algorithm, mode=None, backend=default_backend())
-encryptor = cipher.encryptor()
+fileName= "ejemplo.txt"
 
-#PUT: Encrypt a file into secure folder
-file_to_put= "ejemplo.txt"
-with open(file_to_put, "rb") as source, open(path_secure+file_to_put, "wb+") as sink:
-    byte = source.read(chunk_size)
-    while byte:
-        sink.write(encryptor.update(byte))
-        # Do stuff with byte.
-        byte = source.read(chunk_size)
-    source.close()
-    sink.close()
+nameClient = "testflowww@yahoo.com"
 
-decryptor = cipher.decryptor()
+encrypt_file(nameClient, fileName)
+key_rotation(nameClient)
+decrypt_file(nameClient, fileName)
 
-#GET: Encrypt a file into secure folder
-file_to_get= "ejemplo.txt"
-with open(path_secure+file_to_get, "rb") as source, open(path_unsecure+file_to_get, "wb+") as sink:
-    byte = source.read(chunk_size)
-    while byte:
-        sink.write(decryptor.update(byte))
-        # Do stuff with byte.
-        byte = source.read(chunk_size)
-    source.close()
-    sink.close()
+'''
+nameClient2 = "test2"
+keyClient2 = create_key_client(nameClient2)
+keyFile2 = encrypt_file(keyClient2, nameClient2, fileName)
+
+decrypt_file(keyClient,keyFile,nameClient,fileName)
+decrypt_file(keyClient2,keyFile2,nameClient2,fileName)
+
+newKeyClient = create_key_client(nameClient)
+
+key_rotation(keyClient, newKeyClient, keyFile, nameClient, fileName)
+'''
--- models.py
+++ models.py
@@ -1,5 +1,9 @@
 import pymongo as pymongo
 import hashlib
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+import os, binascii
+from backports.pbkdf2 import pbkdf2_hmac
 
 client = pymongo.MongoClient("mongodb+srv://spea:grupodetres@cluster0.uscnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client['ServerFiles']
@@ -8,7 +12,13 @@ coleccion = db['Usuarios']
 
 def insertar_usuario(correo, password):
     hashed_password = hashlib.new("sha1", password.encode())
-    usuario = {"correo": correo, "password": hashed_password.hexdigest()}
+
+    #Crea la key_client para guardarla con el usuario a crear.
+    salt = binascii.unhexlify('aaef2d3f4d77ac66e9c5a6c3d8f921d1')
+    passwordTmp = password.encode("utf8")
+    key = pbkdf2_hmac("sha256", passwordTmp, salt, 50000, 32)
+
+    usuario = {"correo": correo, "password": hashed_password.hexdigest(), "key": binascii.hexlify(key)}
     resultado = coleccion.insert_one(usuario)
     return resultado
 
