ea124d6eb60e151afbfea6f01595b5e144beb3bc
Alejandro <alexgarohotmailcom@gmail.com>
2021-03-27 10:59:06 +0100

    Update

--- main_ejemplo.py
+++ main_ejemplo.py
@@ -14,11 +14,13 @@ from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives import hashes
 import os, binascii
 from backports.pbkdf2 import pbkdf2_hmac
+from Crypto.Cipher import AES
 
 client = pymongo.MongoClient("mongodb+srv://spea:grupodetres@cluster0.uscnp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client['ServerFiles']
 coleccionUsuarios = db['Usuarios']
 coleccionFicheros = db['Ficheros']
+nonce = bytes("0123456789012345",'utf-8')
 
 def convert_key(shared_key):
     derived_key = HKDF(
@@ -32,28 +34,43 @@ def convert_key(shared_key):
 
     return key
 
-def encrypt_data_key(key_client, filename):
+def encrypt_data_key(key_client, filename, encrypt_option):
 
     salt = binascii.unhexlify('aaef2d3f4d77ac66e9c5a6c3d8f921d1')
     password = filename.encode("utf8")
     key = pbkdf2_hmac("sha256", password, salt, 50000, 32)
 
-    f = Fernet(convert_key(key_client))
-    return f.encrypt(key), key
+    if encrypt_option == 0:
+        f = Fernet(convert_key(key_client))
+        return f.encrypt(key), key
+    else:
+        key_aes = convert_key(key_client)
+        key_aes = key_aes[1:33]
 
-def encrypt_file(client_name, filename):
+        cipher = AES.new(key_aes, AES.MODE_GCM, nonce=nonce)
+        encrypted_message = cipher.encrypt(key) 
+        return encrypted_message, key
+
+def encrypt_file(client_name, filename, encrypt_option):
 
     upload_path = 'upload/' + filename
     encrypted_path = 'encrypted/' + client_name
     key_client = coleccionUsuarios.find_one({"correo": client_name})['key']
 
     with open(upload_path, "rb") as file:
-      file_contents = file.read()
+        file_contents = file.read()
 
-    data_key_encrypted, data_key_plaintext = encrypt_data_key(key_client, upload_path)
+    data_key_encrypted, data_key_plaintext = encrypt_data_key(key_client, upload_path, encrypt_option)
 
-    f = Fernet(convert_key(data_key_plaintext))
-    file_contents_encrypted = f.encrypt(file_contents)
+    if encrypt_option == 0:
+        f = Fernet(convert_key(data_key_plaintext))
+        file_contents_encrypted = f.encrypt(file_contents)
+    else:
+        key_aes = convert_key(data_key_plaintext)
+        key_aes = key_aes[1:33]
+
+        cipher = AES.new(key_aes, AES.MODE_GCM, nonce=nonce)
+        file_contents_encrypted = cipher.encrypt(file_contents) 
 
     os.makedirs(encrypted_path, exist_ok=True)
 
@@ -67,12 +84,19 @@ def encrypt_file(client_name, filename):
     fichero = {"client": client_name, "datakey": data_key_encrypted, "path": encrypted_path + '/' + filename}
     coleccionFicheros.insert_one(fichero)
 
-def decrypt_data_key(data_key_encrypted, key_client):
+def decrypt_data_key(data_key_encrypted, key_client, encrypt_option):
 
-    f = Fernet(convert_key(key_client))
-    return f.decrypt(data_key_encrypted)
+    if encrypt_option == 0:
+        f = Fernet(convert_key(key_client))
+        return f.decrypt(data_key_encrypted)
+    else:
+        key_aes = convert_key(key_client)
+        key_aes = key_aes[1:33]
 
-def decrypt_file(client_name, filename):
+        cipher = AES.new(key_aes, AES.MODE_GCM, nonce=nonce)
+        return cipher.decrypt(data_key_encrypted)
+
+def decrypt_file(client_name, filename, encrypt_option):
 
     encrypted_path = 'encrypted/' + client_name
     decrypted_path = 'download/' + client_name
@@ -80,22 +104,28 @@ def decrypt_file(client_name, filename):
     data_key_encrypted = coleccionFicheros.find_one({"path": encrypted_path + "/" + filename})['datakey']
 
     with open(encrypted_path + '/' + filename, "rb") as file:
-      file_contents = file.read()
+        file_contents = file.read()
 
-    data_key_plaintext = decrypt_data_key(data_key_encrypted, key_client)
+    data_key_plaintext = decrypt_data_key(data_key_encrypted, key_client, encrypt_option)
 
-    f = Fernet(convert_key(data_key_plaintext))
-    file_contents_decrypted = f.decrypt(file_contents)
+    if encrypt_option == 0:
+        f = Fernet(convert_key(data_key_plaintext))
+        file_contents_decrypted = f.decrypt(file_contents)
+    else:
+        key_aes = convert_key(data_key_plaintext)
+        key_aes = key_aes[1:33]
+
+        cipher = AES.new(key_aes, AES.MODE_GCM, nonce=nonce)
+        file_contents_decrypted = cipher.decrypt(file_contents)
 
     os.makedirs(decrypted_path, exist_ok=True)
 
     with open(decrypted_path + '/' + filename, 'wb') as file_decrypted:
-      file_decrypted.write(file_contents_decrypted)
+        file_decrypted.write(file_contents_decrypted)
 
-def key_rotation(client_name):
+def key_rotation(client_name, encrypt_option):
 
     oldKeyClient = coleccionUsuarios.find_one({"correo": client_name})['key']
-    #MIGUEL: How to decode the password???
     password = coleccionUsuarios.find_one({"correo": client_name})['password']
 
     salt = binascii.unhexlify('aaef2d3f4d77ac66e9c5a6c3d8f921d1')
@@ -114,11 +144,18 @@ def key_rotation(client_name):
         with open(encrypted_path + '/' + filename, "rb") as file:
             file_contents = file.read()
       
-        data_key_plaintext = decrypt_data_key(data_key_encrypted, oldKeyClient)
+        data_key_plaintext = decrypt_data_key(data_key_encrypted, oldKeyClient, encrypt_option)
 
-        f = Fernet(convert_key(data_key_plaintext))
-        file_contents_decrypted = f.decrypt(file_contents)
+        if encrypt_option == 0:
+            f = Fernet(convert_key(data_key_plaintext))
+            file_contents_decrypted = f.decrypt(file_contents)
+        else:
+            key_aes = convert_key(data_key_plaintext)
+            key_aes = key_aes[1:33]
 
+            cipher = AES.new(key_aes, AES.MODE_GCM, nonce=nonce)
+            file_contents_decrypted = cipher.decrypt(file_contents)
+        
         os.makedirs(keyrotation_path, exist_ok=True)
 
         with open(keyrotation_path + '/' + filename, 'wb') as file_decrypted:
@@ -129,10 +166,17 @@ def key_rotation(client_name):
         with open(keyrotation_path + '/' + filename, "rb") as file:
             file_contents = file.read()
         
-        data_key_encrypted, data_key_plaintext = encrypt_data_key(newKeyClient, keyrotation_path)
+        data_key_encrypted, data_key_plaintext = encrypt_data_key(newKeyClient, keyrotation_path, encrypt_option)
 
-        f = Fernet(convert_key(data_key_plaintext))
-        file_contents_encrypted = f.encrypt(file_contents)
+        if encrypt_option == 0:
+            f = Fernet(convert_key(data_key_plaintext))
+            file_contents_encrypted = f.encrypt(file_contents)
+        else:
+            key_aes = convert_key(data_key_plaintext)
+            key_aes = key_aes[1:33]
+
+            cipher = AES.new(key_aes, AES.MODE_GCM, nonce=nonce)
+            file_contents_encrypted = cipher.encrypt(file_contents) 
 
         with open(encrypted_path + '/' + filename, 'wb') as file_encrypted:
             file_encrypted.write(file_contents_encrypted)
@@ -142,12 +186,11 @@ def key_rotation(client_name):
         coleccionFicheros.update_one({"path": encrypted_path + "/" + filename},{"$set": {"datakey": data_key_encrypted}})
 
 fileName= "ejemplo.txt"
-
 nameClient = "testflowww@yahoo.com"
 
-encrypt_file(nameClient, fileName)
-key_rotation(nameClient)
-decrypt_file(nameClient, fileName)
+encrypt_file(nameClient, fileName, 1)
+key_rotation(nameClient, 1)
+decrypt_file(nameClient, fileName, 1)
 
 '''
 nameClient2 = "test2"
